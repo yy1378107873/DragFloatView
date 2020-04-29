e9342c68fe3cf0e266bd255fd47074e5353f16d2
songjun <songjun@mobase.cn>
2020-04-29 14:35:26 +0800

    去除无用类

--- floatviewdemo/src/main/java/com/song/floatview/MainActivity.kt
+++ floatviewdemo/src/main/java/com/song/floatview/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.song.kotlinapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * @author : songjun
@@ -11,25 +13,19 @@ import android.widget.TextView
  */
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var textView: TextView;
+    private lateinit var floatView : View
+    private lateinit var ivImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView = findViewById<TextView>(R.id.tv);
-        textView?.postDelayed(
-                Runnable {
-                    var add = add(5, 6);
-                    textView.setText(add.toString())
-                }, 200
-        )
-        textView.setOnClickListener {
-            startActivity(Intent(this,SecondActivity().javaClass))
-//            val intent = Intent();
-//            intent.setClass(this,SecondActivity().javaClass);
+        floatView = layoutInflater.inflate(R.layout.layout_float_view,null,false)
+        ivImage = floatView.findViewById<ImageView>(R.id.iv_image);
+        ivImage.setOnClickListener {
+            Toast.makeText(MainActivity@this,"点击了一下",Toast.LENGTH_SHORT).show()
         }
-    }
+        float_view.setView(floatView
+                , ScreenUtil.widthPixels -ScreenUtil.dpToPx(100f,MainActivity@this)
+                ,ScreenUtil.heightPixels -ScreenUtil.dpToPx(400f,MainActivity@this))
 
-    fun add(a: Int, b: Int): Int {
-        return a + b;
     }
 }
